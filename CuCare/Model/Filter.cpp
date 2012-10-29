25de28afd0568a5f74af75d02f9ee14f190b11a3
Sergey Vershinin <svershin@gmail.com>
2012-10-29 14:57:34 -0400

    blah

---
+++ CuCare/Model/Filter.cpp
@@ -0,0 +1,24 @@
+// COMP 3004 FALL 2012
+// Assignment 2: cuCare Prototype
+// Team: The Four Puppeteers
+// Contributing Editors: Sergey Vershinin
+//
+// Filter.h - Declaration of base class Filter
+// Member functions are defined in Filter.cpp
+
+#include "Filter.h"
+
+// Constructor
+Filter::Filter
+{
+}
+
+// Destructor
+Filter::~Filter()
+{
+}
+
+// int Filter::getFIELD() { }
+// void Filter::setFIELD( int FIELD ) { }
+
+// EOF
