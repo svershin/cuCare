ba341eb6a518ba30c3ed6e6c0a053ebf0269cb6a
Mike <mike@ubuntu.(none)>
2012-11-03 19:20:52 -0400

    request.h parameters modified again

--- CuCareCommunications/Communications/request.h
+++ CuCareCommunications/Communications/request.h
@@ -38,19 +38,19 @@ public:
     int createSysAdmin(string *pErrString, SysAdmin *pInputSysAdmin, int *pUid);
     int createPatient(string *pErrString, Patient *pInputPatient, int physicianId, int *pUid);
     int createConsultation(string *pErrString, Consultation *pInputConsultation, int physicianId, int patientId, int *pUid);
-    int createReferral(string *pErrString, Referral *pInputReferral, int consultationId, int **ppUid);
+    int createReferral(string *pErrString, Referral *pInputReferral, int consultationId, int *pUid);
     int createMedicalTest(string *pErrString, MedicalTest *pInputMedicalTest, int consultationId, int **ppUid);
     int createReturnConsultation(string *pErrString, ReturnConsultation *pInputReturnConsultation, int consultationId, int nextConsultationId, int *pUid);
     int createMedicationRenewal(string *pErrString, MedicationRenewal *pInputMedicationRenewal, int consultationId, int *pUid);
 
     //Modification functions
-    int pushUser(string *pErrString, User inputUser);
-    int pushPatient(string *pErrString, Patient inputPatient, int physicianId);
-    int pushConsultation(string *pErrString, Consultation inputPatient, int physicianId, int patientId);
-    int pushReferral(string *pErrString, Referral inputReferral, int consultationId);
-    int pushMedicalTest(string *pErrString, MedicalTest inputMedicalTest, int physicianId);
-    int pushReturnConsultation(string *pErrString, ReturnConsultation inputReturnConsultation, int physicianId, int nextConsultationId);
-    int pushMedicationRenewal(string *pErrString, MedicationRenewal inputMedicationRenewal, int physicianId);
+    int pushUser(string *pErrString, User *pInputUser);
+    int pushPatient(string *pErrString, Patient *pInputPatient, int physicianId);
+    int pushConsultation(string *pErrString, Consultation *pInputPatient, int physicianId, int patientId);
+    int pushReferral(string *pErrString, Referral *pInputReferral, int consultationId);
+    int pushMedicalTest(string *pErrString, MedicalTest *pInputMedicalTest, int physicianId);
+    int pushReturnConsultation(string *pErrString, ReturnConsultation *pInputReturnConsultation, int physicianId, int nextConsultationId);
+    int pushMedicationRenewal(string *pErrString, MedicationRenewal *pInputMedicationRenewal, int physicianId);
 
     //Object retrieval functions
     int pullPhysician(string *pErrString, Physician *pPhysicianValues, UserFilter inputFilter, vector<Physician*> *pOutputPhysicians);
