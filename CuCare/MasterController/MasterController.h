0413bac6578364e44b6fc96d6f9d0d7c931ae0b4
Sergey Vershinin <svershin@gmail.com>
2012-11-04 11:23:53 -0500

    An alsmot-completed revision of MasterController.h

--- CuCare/MasterController/MasterController.h
+++ CuCare/MasterController/MasterController.h
@@ -29,42 +29,65 @@ public:
     };
 
     AccessControlStatus loginUser(string username, string *pErrString);
-    AccessControlStatus loginStatus();
+    AccessControlStatus loginStatus(); // see AccessControlStatus above for return codes
     AccessControlStatus logout();
 
-    // Patients
+    /* Return types for methods below:
+       0 - FAIL
+       1 - SUCCESS
+    */
 
-    // createPatient() Parameters:
-    // pInputPatient - a ptr to a Patient object with the new data (do not delete, this will get assigned to currentPatient)
-    // physicianId - the UID of the Patient's Physician
-    // pErrString - a ptr to a string that will be populated with the error message (if any)
+    // PATIENT MANAGEMENT
+
+    /* createPatient() - Creates a new patient in storage, and sets pCurrentPatient
+       pInputPatient - A ptr to a Patient object with the new data (DO NOT DELETE, this will get assigned to currentPatient)
+       physicianId - The UID of the Patient's Physician (HINT: will need to call getPhysicianList() first)
+       pErrString - A ptr to a string that will be populated with the error message (if any)
+    */
     bool createPatient(Patient* pInputPatient, int physicianId, string *pErrString);
 
-    bool modifyPatient(Patient* pInputPatient, int physicianId, string *pErrString);
+    /* modifyPatient() - Pushes changes to pCurrentPatient to storage
+       pErrString - A ptr to a string that will be populated with the error message (if any)
+    */
 
+    bool modifyPatient(string *pErrString);
+
+    /* getPatientList() - Fetches a list of "shallow" Patient objects from storage
+       pResults - Ptr to a vector of Patient ptrs, will get populated with data from storage
+       pErrString - A ptr to a string that will be populated with the error message (if any)
+    */
     bool getPatientList(vector<Patient *> *pResults, string *pErrString); // no filter
 
-//    bool getPatientList(Patient* pPatientValues, PatientFilter patientFilter, vector<Patient*>* pResults); // filtered, will not populate consultations or followups
+    /* NOTES: This is is not required for the prototype, will allow filtered pulling of patients
+       bool getPatientList(Patient* pPatientValues, PatientFilter patientFilter, vector<Patient*>* pResults, string *pErrString);
+    */
 
-    // getFullPatient() Notes:
-    // ? gets the full record for the patient, and points pCurrentPatient
-    // ? assumes that it is being passed a pointer to a "shallow" patient that comes from a getPatientList() call
-    bool getFullPatient(Patient* newCurrentPatient, string *pErrString); // must supply valid UID
+    /* setCurrentPatient() - Fetches a full patient record (including consultations and followups) from storage
+       patientId - A valid UID for a patient record (HINT: will need to call getPatientList() first)
+       pErrString - A ptr to a string that will be populated with the error message (if any)
+    */
+    bool getFullPatient(int patientId, string *pErrString);
 
-    Patient* getCurrentPatient();   // returns a pointer to the currently selected patient
-                                    // NULL ptr if no patient has yet been selected
+    /* getCurrentPatient() - Returns a pointer to the currently selected patient
+       Note: Returns a NULL ptr if no patient has yet been selected (using setCurrentPatient)
+    */
+    Patient* getCurrentPatient();
 
-    // Consultations
+    // CONSULTATIONS
 
     bool createConsultation(Consultation* pInputConsultation, string *pErrString);
     bool modifyConsultation(Consultation* pInputConsultation, string *pErrString);
 
-    // Follow-ups
+    // FOLLOWUPS
 
     bool createFollowup(Followup* pInputFollowup, string *pErrString);
     bool modifyFollowup(Followup* pInputFollowup, string *pErrString);
 
-    // Physicians
+    // PHYSICIANS
+
+    /* getPhysicianList() - Fetches a list of "shallow" Physician objects from storage
+       pResults - Ptr to a vector of Physician ptrs; will get populated with data from storage
+    */
 
     bool getPhysicianList(vector<Physician*> *pResults, string *pErrString);
 
@@ -76,6 +99,7 @@ private:
     // Set to NULL if not logged-in
     User* pCurrentUser;
 
+    //
     AccessControlStatus status;
 
     // Stores the data for the currently selected patient
