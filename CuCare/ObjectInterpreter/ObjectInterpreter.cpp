1e08a6fbf03033768f61e42c1ade3c9d36c377d6
Devin Denis <ddenis@connect.carleton.ca>
2012-11-30 18:49:44 -0500

    Added objectInterpreter folder

---
+++ CuCare/ObjectInterpreter/ObjectInterpreter.cpp
@@ -0,0 +1,30 @@
+#include "ObjectInterpreter.h"
+#include "../../CuCareModel/Property.h"
+
+ObjectInterpreter::ObjectInterpreter()
+{
+}
+
+void ObjectInterpreter::ObjectToProperties(ModelObject *modelObject, map<string, string> *properties)
+{
+    list<Property*>* objectProps = modelObject->getProperties();
+
+    for(list<Property*>::iterator it = objectProps->begin(); it != objectProps->end(); ++it)
+        (*properties)[(*it)->getName()] = (*it)->getValue();
+
+    //TODO: find out if Mike takes tableName seperately or if I'm supposed to put it in the map
+    (*properties)[OBJECT_TYPE_PROPERTY_NAME] = modelObject->getObjectType();
+    (*properties)[ID_NAME_PROPERTY_NAME] = modelObject->getIdName();
+}
+
+void ObjectInterpreter::PropertiesToObject(map<string, string> *properties, ModelObject *modelObject)
+{
+    list<Property*>* objectProps = modelObject->getProperties();
+
+    //Object type and id name are not regular properties, and should be handled outside this function.
+    properties->erase(OBJECT_TYPE_PROPERTY_NAME);
+    properties->erase(ID_NAME_PROPERTY_NAME);
+
+    for(list<Property*>::iterator it = objectProps->begin(); it != objectProps->end(); ++it)
+        (*it)->setValue((*properties)[(*it)->getName()]);
+}
